c07194d75e680d9d21cd4b1ec74209cb8010136a
ikecruz <ikedinobicruz7@gmail.com>
2024-04-27 11:21:05 +0100

    fixes and additions

--- src/services/receipt.service.ts
+++ src/services/receipt.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Receipt } from "@prisma/client";
 import { CreateReceiptDto, GetReceiptDto } from "../dtos/receipt.dto";
 import { MediaService } from "./media.service";
 import { UserService } from "./user.service";
@@ -6,23 +6,31 @@ import database from "../database";
 import HttpException from "../utils/exception";
 import { StatusCodes } from "http-status-codes";
 import { QueryFilter } from "../interfaces/query.interface";
+import moment from "moment";
+import NotificationService from "./notification.service";
 
 export class ReceiptService {
 
     private readonly mediaService: MediaService;
     private readonly userService: UserService;
     private readonly ormService: PrismaClient;
+    private readonly notificationService: NotificationService;
 
     constructor() {
         this.userService = new UserService()
         this.mediaService = new MediaService();
+        this.notificationService = new NotificationService();
         this.ormService = database.getClient()
     }
 
     async create(dto: CreateReceiptDto, businessId: number, file: Express.Multer.File) {
 
         const token = await this.userService.validateShareCode(dto.share_code);
-
+        const user = await this.ormService.user.findFirst({
+            where: {
+                id: token.user_id
+            }
+        })
         const filePath = await this.mediaService.uploadReceipt(file);
 
         await this.ormService.receipt.create({
@@ -33,8 +41,34 @@ export class ReceiptService {
             }
         })
 
+        if (user?.push_token) {
+            this.notificationService.sendNotification(
+                [user.push_token],
+                "Your Receipt Upload: Complete!",
+                "Great news! Your recent receipt upload was successful. Keep track of your expenses effortlessly"
+            )
+        }
+
         return "Receipt Created Successfully"
 
+    }
+
+    private formatReceipts(receipts: Receipt[]) {
+        const groups = receipts.reduce((groups: any, receipt: Receipt) => {
+            const date = moment(receipt.created_at).format('YYYY-MM-DD')
+            if (!groups[date]) {
+                groups[date] = []
+            }
+            groups[date].push(receipt)
+            return groups
+        }, {})
+        const groupArrays = Object.keys(groups).map((date) => {
+            return {
+                date,
+                transactions: groups[date]
+            }
+        })
+        return groupArrays
     }
 
     async get(userId: number, dto: GetReceiptDto) {
@@ -95,7 +129,7 @@ export class ReceiptService {
         })
         
         return { 
-            results: receipts,
+            results: this.formatReceipts(receipts),
             limit 
         }
 
